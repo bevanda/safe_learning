ceedeef312cc1e8dac9fdda0f8d4c2d072106877
PPENVYX360\petar <petar.bevandaex@gmail.com>
2019-02-08 03:37:39 +0100

    Added mass spring damper model to compute and set up ipynb for testing

--- safe_learning/compute.py
+++ safe_learning/compute.py
@@ -16,7 +16,7 @@ if sys.version_info.major == 2:
     import imp
 
 __all__ = ['compute_roa', 'binary_cmap', 'constrained_batch_sampler', 'compute_closedloop_response',
-           'get_parameter_change', 'import_from_directory', 'LyapunovNetwork', 'InvertedPendulum', 'CartPole']
+           'get_parameter_change', 'import_from_directory', 'LyapunovNetwork', 'InvertedPendulum', 'CartPole', 'MassSpringDamper']
 
 NP_DTYPE = config.np_dtype
 TF_DTYPE = config.dtype
@@ -435,6 +435,147 @@ class CartPole(DeterministicFunction):
 
         state_derivative = tf.concat((x_dot, theta_dot, v_dot, omega_dot), axis=1)
 
+        return state_derivative
+
+class MassSpringDamper(DeterministicFunction):
+    """Mass-Spring-Damper.
+
+    Parameters
+    ----------
+    mass : float
+    friction : float, optional
+    dt : float, optional
+        The sampling time.
+    normalization : tuple, optional
+        A tuple (Tx, Tu) of arrays used to normalize the state and actions. It
+        is so that diag(Tx) *x_norm = x and diag(Tu) * u_norm = u.
+
+    """
+
+    def __init__(self, mass, elastic_const, friction=0, dt=1 / 80,
+                 normalization=None):
+        """Initialization; see `MassSpringDamper`."""
+        super(MassSpringDamper, self).__init__()
+        self.mass = mass
+        self.gravity = 9.81
+        self.elastic_const = elastic_const
+        self.friction = friction
+        self.dt = dt
+
+        self.normalization = normalization
+        if normalization is not None:
+            self.normalization = [np.array(norm, dtype=config.np_dtype)
+                                  for norm in normalization]
+            self.inv_norm = [norm ** -1 for norm in self.normalization]
+
+    def normalize(self, state, action):
+        """Normalize states and actions."""
+        if self.normalization is None:
+            return state, action
+
+        Tx_inv, Tu_inv = map(np.diag, self.inv_norm)
+        state = tf.matmul(state, Tx_inv)
+
+        if action is not None:
+            action = tf.matmul(action, Tu_inv)
+
+        return state, action
+
+    def denormalize(self, state, action):
+        """De-normalize states and actions."""
+        if self.normalization is None:
+            return state, action
+
+        Tx, Tu = map(np.diag, self.normalization)
+
+        state = tf.matmul(state, Tx)
+        if action is not None:
+            action = tf.matmul(action, Tu)
+
+        return state, action
+
+    def linearize(self):
+        """Return the linearized system.
+
+        Returns
+        -------
+        a : ndarray
+            The state matrix.
+        b : ndarray
+            The action matrix.
+
+        """
+        gravity = self.gravity
+        friction = self.friction
+        elastic_const = self.elastic_const
+        inertia = self.mass
+
+        A = np.array([[0, 1],
+                      [-elastic_const / inertia, -friction/ inertia]],
+                     dtype=config.np_dtype)
+
+        B = np.array([[0],
+                      [1 / inertia]],
+                     dtype=config.np_dtype)
+
+        if self.normalization is not None:
+            Tx, Tu = map(np.diag, self.normalization)
+            Tx_inv, Tu_inv = map(np.diag, self.inv_norm)
+
+            A = np.linalg.multi_dot((Tx_inv, A, Tx))
+            B = np.linalg.multi_dot((Tx_inv, B, Tu))
+
+        sys = signal.StateSpace(A, B, np.eye(2), np.zeros((2, 1)))
+        sysd = sys.to_discrete(self.dt)
+        return sysd.A, sysd.B
+
+    @concatenate_inputs(start=1)
+    def build_evaluation(self, state_action):
+        """Evaluate the dynamics."""
+        # Denormalize
+        state, action = tf.split(state_action, [2, 1], axis=1)
+        state, action = self.denormalize(state, action)
+
+        n_inner = 10
+        dt = self.dt / n_inner
+        for i in range(n_inner):
+            state_derivative = self.ode(state, action)
+            state = state + dt * state_derivative
+
+        return self.normalize(state, None)[0]
+
+    def ode(self, state, action):
+        """Compute the state time-derivative.
+
+        Parameters
+        ----------
+        states: ndarray or Tensor
+            Unnormalized states.
+        actions: ndarray or Tensor
+            Unnormalized actions.
+
+        Returns
+        -------
+        x_dot: Tensor
+            The normalized derivative of the dynamics
+
+        """
+        # Physical dynamics
+        gravity = self.gravity
+        friction = self.friction
+        elastic_const = self.elastic_const
+        inertia = self.mass
+
+        position, velocity = tf.split(state, 2, axis=1)
+
+        x_ddot = -(elastic_const / inertia)*position + action / inertia
+
+        if friction > 0:
+            x_ddot -= (friction / inertia)  * velocity
+
+        state_derivative = tf.concat((velocity, x_ddot), axis=1)
+
+        # Normalize
         return state_derivative
 
 
